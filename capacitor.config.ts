66f10a26562f31af5b2aae26cdae54c580032ff4
Nazar Mykhalkevych <nazar.mykhalkevych@UALWO9724NBM.LEVI9.COM>
2024-10-10 17:59:24 +0300

    add ios icon app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.carauction',
   appName: 'carauction',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
